20334d4345ccac1c45f9c3fa69203c76bccc42e4
sunnyregion <jinheking@sina.com>
2016-06-06 14:11:02 +0800

    03源程序

---
+++ src/go_lang_base/03/pro03_2_1.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	var i int
+	var j int32
+	i = 2
+	j = i
+}
---
+++ src/go_lang_base/03/pro03_2_2.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int
+	var j int32
+	i = 2
+	j = int32(i)
+	fmt.Println(j)
+}
